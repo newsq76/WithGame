9be448bebcf795fbe4746186b401bf85d63dbcb0
newsq <newpx01@gmail.com>
2021-01-16 09:34:25 +0000

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
  root to: 'toppages#index'
+ 
+ get 'edit_profile', to: 'users#edit'
+ get 'signup', to: 'users#new'
+ resources :users, only: [:index, :show, :new, :create, :edit, :update]
 end
